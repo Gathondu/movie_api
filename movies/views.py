ac13564878af8e947b53bbe73e356946b0ce4ebe
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2019-07-04 00:31:44 +0300

    use assignment instead of extend
    Signed-off-by: Denis Ngugi Gathondu <denis.gathondu@yahoo.com>

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,8 @@
-from data import movies
 from django.http import JsonResponse
+
+from data import movies
 from movies.utils import binarySearch, linearSearch
+
 
 def index(request):
     return JsonResponse({ 'count': len(movies), 'movies': movies})
@@ -12,11 +14,10 @@ def get_movie(request, movie_id):
     return JsonResponse({'error': 'No movie with that id'}, status=404)
 
 def search(request):
-    results = []
     keys = [key.lower() for key in request.GET.keys() if key.lower() in ('name', 'genre')]
     if len(keys) == 1:
         query = request.GET.get(keys[0], None)
-        results.extend(linearSearch(movies, keys[0], query))
+        results = linearSearch(movies, keys[0], query)
     else:
         return JsonResponse({'error': 'You can only search name or genre of a movie.'}, status=400)
     return JsonResponse({'count': len(results), 'movies': results})

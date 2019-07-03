50b4b50813ac040e9ffda4444ae58e76ac92df96
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2019-07-03 21:50:14 +0300

    Add search binary search for id
    Signed-off-by: Denis Ngugi Gathondu <denis.gathondu@yahoo.com>

---
+++ movies/utils.py
@@ -0,0 +1,22 @@
+# return the index where the id lies if present, else false
+def binarySearch(movies, start, end, id):
+    # check base case
+    if start <= end:
+        mid = int((start + (end))/2)
+        if movies[mid]['id'] == id:
+            return movies[mid]
+        # if movie id is smaller than mid, then it's in the left subarray
+        elif movies[mid]['id'] > id:
+            return binarySearch(movies, start, mid-1, id)
+        # else it can only be in the right subarray
+        else:
+            return binarySearch(movies, mid+1, end, id)
+    else:
+        # element is not present
+        return False
+
+def linearSearch(movies, id):
+    for movie in movies:
+        if movie['id'] == id:
+            return movie
+    return False
--- movies/views.py
+++ movies/views.py
@@ -1,11 +1,12 @@
 from data import movies
 from django.http import JsonResponse
+from movies.utils import binarySearch
 
 def index(request):
     return JsonResponse(movies, safe=False)
 
 def get_movie(request, movie_id):
-    for movie in movies:
-        if movie['id'] == movie_id:
-            return JsonResponse(movie)
+    movie = binarySearch(movies, 0, len(movies)-1, movie_id)
+    if movie:
+        return JsonResponse(movie)
     return JsonResponse({'error': 'No movie with that id'}, status=404)

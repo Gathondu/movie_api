1bc77bd8e1445dbac4e873870d211290a76acfe1
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2019-07-04 01:30:10 +0300

    Refactor tests

--- movies/tests.py
+++ movies/tests.py
@@ -1,8 +1,11 @@
 import json
 
-from data import movies
 from django.test import SimpleTestCase
 from django.utils.encoding import force_text
+
+from data import movies
+from movies.utils import binarySearch, linearSearch
+
 
 class MovieTestCase(SimpleTestCase):
     def setUp(self):
@@ -12,12 +15,13 @@ class MovieTestCase(SimpleTestCase):
         response = self.client.get('/api/v1/movies/')
         data = json.loads(response.content)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(data['count'], 1000)
+        self.assertEqual(data['count'], len(self.movies))
 
     def test_we_get_specific_movie(self):
         response = self.client.get('/api/v1/movies/1')
+        movie = binarySearch(self.movies, 0, len(self.movies) - 1, 1)
         self.assertEqual(response.status_code, 200)
-        self.assertJSONEqual(force_text(response.content), self.movies[0])
+        self.assertJSONEqual(force_text(response.content), movie)
 
     def test_unavailable_movie_id_fails(self):
         response = self.client.get('/api/v1/movies/2001')
@@ -28,21 +32,17 @@ class MovieTestCase(SimpleTestCase):
         response = self.client.get('/api/v1/movies/search?name=ton')
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.content)
-        # searching ton should get us 8 results of movies with the substring ton
-        self.assertEqual(data['count'], 8)
-        # Since we are using a linear search, the 1st item matching this criterion
-        # is at index 8
-        self.assertEqual(data['movies'][0], self.movies[8])
+        results = linearSearch(self.movies, 'name', 'ton')
+        self.assertEqual(data['count'], len(results))
+        self.assertEqual(data['movies'][0], results[0])
 
     def test_search_for_genre(self):
         response = self.client.get('/api/v1/movies/search?genre=drama')
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.content)
-        # searching drama should get us 493 results of movies with the genre drama
-        self.assertEqual(data['count'], 493)
-        # Since we are using a linear search, the 1st item matching this criterion
-        # is at index 0
-        self.assertEqual(data['movies'][0], self.movies[0])
+        results = linearSearch(self.movies, 'genre', 'drama')
+        self.assertEqual(data['count'], len(results))
+        self.assertEqual(data['movies'][0], results[0])
 
     def test_search_using_wrong_key(self):
         response = self.client.get('/api/v1/movies/search?showing_time=10:18')

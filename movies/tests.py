a5021b3f31bb61414fe58a420248b6c86c7cdbc1
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2019-07-03 17:56:18 +0300

    Add edge case test for individual movie
    Signed-off-by: Denis Ngugi Gathondu <denis.gathondu@yahoo.com>

--- movies/tests.py
+++ movies/tests.py
@@ -14,8 +14,12 @@ class MovieTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(data), 1000)
 
-
     def test_we_get_specific_movie(self):
         response = self.client.get('/movie/1')
         self.assertEqual(response.status_code, 200)
         self.assertJSONEqual(force_text(response.content), self.movies[0])
+
+    def test_unavailable_movie_id_fails(self):
+        response = self.client.get('/movie/2001')
+        self.assertEqual(response.status_code, 404)
+        self.assertJSONEqual(force_text(response.content), {'error': 'No movie with that id'})

fe6d90f55420bc2baafea8ea38b572f9cdc6ff97
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2019-07-03 17:33:26 +0300

    Add test for getting one movie
    Signed-off-by: Denis Ngugi Gathondu <denis.gathondu@yahoo.com>

--- movies/tests.py
+++ movies/tests.py
@@ -1,10 +1,21 @@
 import json
 
+from data import movies
 from django.test import TestCase
+from django.utils.encoding import force_text
 
 class MovieTestCase(TestCase):
+    def setUp(self):
+        self.movies = movies
+
     def test_we_get_all_movies(self):
         response = self.client.get('/movies/')
         data = json.loads(response.content)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(data), 1000)
+
+
+    def test_we_get_specific_movie(self):
+        response = self.client.get('/movie/1')
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(force_text(response.content), self.movies[0])

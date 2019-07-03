2a09a1ed3fe34d9d85abbd2d5c24a850398257ad
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2019-07-03 17:25:45 +0300

    Add index route test
    Signed-off-by: Denis Ngugi Gathondu <denis.gathondu@yahoo.com>

--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,10 @@
+import json
+
 from django.test import TestCase
 
-# Create your tests here.
+class MovieTestCase(TestCase):
+    def test_we_get_all_movies(self):
+        response = self.client.get('/movies/')
+        data = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(data), 1000)
